e03e0ea9fb4446f58d2926be957b01a3b3b25fe8
CraigFrances <89369191+CraigFrances@users.noreply.github.com>
2021-11-12 11:00:11 -0600

    Completed checkbox functionality

--- Franceschetti.Craig.RRCAGApp/SalesQuoteForm.cs
+++ Franceschetti.Craig.RRCAGApp/SalesQuoteForm.cs
@@ -14,7 +14,15 @@ namespace Franceschetti.Craig.RRCAGApp
 {
     public partial class SalesQuoteForm : Form
     {
-        SalesQuote salesQuote;
+        private SalesQuote salesQuote;
+        private decimal vehicleSalePrice;
+        private decimal tradeInValue;
+        private decimal options;
+        private decimal rate;
+        private int paymentPeriods;
+        private decimal presentValue;
+        private decimal monthlyPaymentOutput;
+
         public SalesQuoteForm()
         {
             InitializeComponent();
@@ -24,20 +32,42 @@ namespace Franceschetti.Craig.RRCAGApp
             this.btnCalculateQuote.Click += BtnCalculateQuote_Click;
             this.txtVehicleSalePrice.TextChanged += TxtVehicleSalePriceOrTxtTradeInValue_TextChanged;
             this.txtTradeInValue.TextChanged += TxtVehicleSalePriceOrTxtTradeInValue_TextChanged;
-            this.radStandard.CheckedChanged += RadStandard_CheckedChanged;
+            this.chkStereoSystem.Click += ChkStereoSystem_Click;
+            this.chkLeatherInterior.Click += ChkLeatherInterior_Click;
+            this.chkComputerNavigation.Click += ChkComputerNavigation_Click;
             
         }
-        private void RadStandard_CheckedChanged(object sender, EventArgs e)
+
+        private void ChkComputerNavigation_Click(object sender, EventArgs e)
         {
-            if (this.errorProvider.GetError(txtVehicleSalePrice).Equals(string.Empty) && this.errorProvider.GetError(this.txtTradeInValue).Equals(string.Empty))
+            if (salesQuote != null && !lblMonthlyPaymentFinanceOutput.Text.Equals(string.Empty))
             {
-                
+                AccessoriesChosen();
+                VehicleSummaryOutputAndFinanceSummaryOutputExcludingTradeIn();
+            }
+        }
+
+        private void ChkLeatherInterior_Click(object sender, EventArgs e)
+        {
+            if (salesQuote != null && !lblMonthlyPaymentFinanceOutput.Text.Equals(string.Empty))
+            {
+                AccessoriesChosen();
+                VehicleSummaryOutputAndFinanceSummaryOutputExcludingTradeIn();
+            }
+        }
+
+        private void ChkStereoSystem_Click(object sender, EventArgs e)
+        {
+            if (salesQuote != null && !lblMonthlyPaymentFinanceOutput.Text.Equals(string.Empty))
+            {
+                AccessoriesChosen();
+                VehicleSummaryOutputAndFinanceSummaryOutputExcludingTradeIn();
             }
         }
 
         private void TxtVehicleSalePriceOrTxtTradeInValue_TextChanged(object sender, EventArgs e)
         {
-            ClearSummaryAndFinanceOutput();
+            ClearVehicleSummaryAndFinanceOutput();
         }
 
         private void SalesQuoteForm_Load(object sender, EventArgs e)
@@ -49,12 +79,12 @@ namespace Franceschetti.Craig.RRCAGApp
         {
             this.txtVehicleSalePrice.Text = string.Empty;
             this.txtTradeInValue.Text = "0";
-            ClearSummaryAndFinanceOutput();
+            ClearVehicleSummaryAndFinanceOutput();
             this.errorProvider.SetIconPadding(this.txtVehicleSalePrice, 3);
             this.errorProvider.SetIconPadding(this.txtTradeInValue, 3);
         }
 
-        private void ClearSummaryAndFinanceOutput()
+        private void ClearVehicleSummaryAndFinanceOutput()
         {
             this.lblVehicleSalePriceOutput.Text = string.Empty;
             this.lblOptionsOutput.Text = string.Empty;
@@ -68,17 +98,14 @@ namespace Franceschetti.Craig.RRCAGApp
 
         private void BtnCalculateQuote_Click(object sender, EventArgs e)
         {
-            decimal vehicleSalePrice;
-            decimal tradeInValue;
-            
             this.errorProvider.SetError(this.txtVehicleSalePrice, string.Empty);
             this.errorProvider.SetError(this.txtTradeInValue, string.Empty);
 
             try
             {
-                vehicleSalePrice = Decimal.Parse(this.txtVehicleSalePrice.Text);
+                this.vehicleSalePrice = Decimal.Parse(this.txtVehicleSalePrice.Text);
 
-                if (vehicleSalePrice <= 0m)
+                if (this.vehicleSalePrice <= 0m)
                 {
                     this.errorProvider.SetError(this.txtVehicleSalePrice, "Vehicle price cannot be less than or equal to 0.");
                 }
@@ -97,9 +124,9 @@ namespace Franceschetti.Craig.RRCAGApp
             
             try
             {
-                tradeInValue = Decimal.Parse(this.txtTradeInValue.Text);
+                this.tradeInValue = Decimal.Parse(this.txtTradeInValue.Text);
 
-                if (tradeInValue < 0)
+                if (this.tradeInValue < 0)
                 {
                     this.errorProvider.SetError(this.txtTradeInValue, "Trade-in value cannot be less than 0.");
                 }
@@ -118,91 +145,117 @@ namespace Franceschetti.Craig.RRCAGApp
 
             if (this.errorProvider.GetError(txtVehicleSalePrice).Equals(string.Empty) && this.errorProvider.GetError(txtTradeInValue).Equals(string.Empty))
             {
-                vehicleSalePrice = Decimal.Parse(this.txtVehicleSalePrice.Text);
-                tradeInValue = Decimal.Parse(this.txtTradeInValue.Text);
-                if(tradeInValue > vehicleSalePrice)
+                this.vehicleSalePrice = Decimal.Parse(this.txtVehicleSalePrice.Text);
+                this.tradeInValue = Decimal.Parse(this.txtTradeInValue.Text);
+                if(this.tradeInValue > this.vehicleSalePrice)
                 {
-                    errorProvider.SetError(txtTradeInValue, "Trade-in value cannot exceed the vehicle sale price.");
-                    ClearSummaryAndFinanceOutput();
+                    errorProvider.SetError(this.txtTradeInValue, "Trade-in value cannot exceed the vehicle sale price.");
+                    ClearVehicleSummaryAndFinanceOutput();
                 }
                 else
                 {
                     const decimal SalesTaxRate = .12m;
-                    salesQuote = new SalesQuote(vehicleSalePrice, tradeInValue, SalesTaxRate);
-
-                    lblVehicleSalePriceOutput.Text = salesQuote.VehicleSalePrice.ToString("C");
-
-                    if (chkStereoSystem.Checked && !chkLeatherInterior.Checked && !chkComputerNavigation.Checked)
-                    {
-                        salesQuote.AccessoriesChosen = Accessories.StereoSystem;
-                    }
-                    else if (chkLeatherInterior.Checked && !chkStereoSystem.Checked && !chkComputerNavigation.Checked)
-                    {
-                        salesQuote.AccessoriesChosen = Accessories.LeatherInterior;
-                    }
-                    else if (chkComputerNavigation.Checked && !chkLeatherInterior.Checked && !chkStereoSystem.Checked)
-                    {
-                        salesQuote.AccessoriesChosen = Accessories.ComputerNavigation;
-                    }
-                    else if (chkStereoSystem.Checked && chkLeatherInterior.Checked && !chkComputerNavigation.Checked)
-                    {
-                        salesQuote.AccessoriesChosen = Accessories.StereoAndLeather;
-                    }
-                    else if (chkStereoSystem.Checked && chkComputerNavigation.Checked && !chkLeatherInterior.Checked)
-                    {
-                        salesQuote.AccessoriesChosen = Accessories.StereoAndNavigation;
-                    }
-                    else if (chkLeatherInterior.Checked && !chkStereoSystem.Checked && chkComputerNavigation.Checked)
-                    {
-                        salesQuote.AccessoriesChosen = Accessories.LeatherAndNavigation;
-                    }
-                    else if (chkLeatherInterior.Checked && chkComputerNavigation.Checked && chkStereoSystem.Checked)
-                    {
-                        salesQuote.AccessoriesChosen = Accessories.All;
-                    }
-                    else
-                    {
-                        salesQuote.AccessoriesChosen = Accessories.None;
-                    }
-
-                    if (radStandard.Checked)
-                    {
-                        salesQuote.ExteriorFinishChosen = ExteriorFinish.Standard;
-                    }
-                    else if (radPearlized.Checked)
-                    {
-                        salesQuote.ExteriorFinishChosen = ExteriorFinish.Pearlized;
-                    }
-                    else if (radCustomizedDetailing.Checked)
-                    {
-                        salesQuote.ExteriorFinishChosen = ExteriorFinish.Custom;
-                    }
-
-                    decimal options = salesQuote.AccessoryCost + salesQuote.FinishCost;
-                    lblOptionsOutput.Text = options.ToString("N");
-
-                    lblSubTotalOutput.Text = salesQuote.SubTotal.ToString("C");
-                    lblSalesTaxOutput.Text = salesQuote.SalesTax.ToString("N");
-                    lblTotalOutput.Text = salesQuote.Total.ToString("C");
-
-                    decimal tradeInAsNegative = -1m * salesQuote.TradeInAmount;
-                    lblTradeInOutput.Text = tradeInAsNegative.ToString("N");
-
-                    lblAmountDueOutput.Text = salesQuote.AmountDue.ToString("C");
-
-                    decimal rate = nudAnnualInterestRate.Value / 100;
-                    int paymentPeriods = (int)nudNumberOfYears.Value * 12;
-                    decimal presentValue = salesQuote.AmountDue;
-                    decimal monthlyPaymentOutput = Financial.GetPayment(rate, paymentPeriods, presentValue);
-
-                    lblMonthlyPaymentFinanceOutput.Text = monthlyPaymentOutput.ToString("C");
+                    salesQuote = new SalesQuote(this.vehicleSalePrice, this.tradeInValue, SalesTaxRate);
+                    AccessoriesChosen();
+                    ExteriorFinishChosen();
+                    VehicleSummaryOutput();
+                    FinancialOutput();
                 }
             } 
             else
             {
-                ClearSummaryAndFinanceOutput();
+                ClearVehicleSummaryAndFinanceOutput();
             }
             
+        }
+
+        private void ExteriorFinishChosen()
+        {
+            if (radStandard.Checked)
+            {
+                salesQuote.ExteriorFinishChosen = ExteriorFinish.Standard;
+            }
+            else if (radPearlized.Checked)
+            {
+                salesQuote.ExteriorFinishChosen = ExteriorFinish.Pearlized;
+            }
+            else if (radCustomizedDetailing.Checked)
+            {
+                salesQuote.ExteriorFinishChosen = ExteriorFinish.Custom;
+            }
+        }
+
+        private void AccessoriesChosen()
+        {
+            if (chkStereoSystem.Checked && !chkLeatherInterior.Checked && !chkComputerNavigation.Checked)
+            {
+                salesQuote.AccessoriesChosen = Accessories.StereoSystem;
+            }
+            else if (chkLeatherInterior.Checked && !chkStereoSystem.Checked && !chkComputerNavigation.Checked)
+            {
+                salesQuote.AccessoriesChosen = Accessories.LeatherInterior;
+            }
+            else if (chkComputerNavigation.Checked && !chkLeatherInterior.Checked && !chkStereoSystem.Checked)
+            {
+                salesQuote.AccessoriesChosen = Accessories.ComputerNavigation;
+            }
+            else if (chkStereoSystem.Checked && chkLeatherInterior.Checked && !chkComputerNavigation.Checked)
+            {
+                salesQuote.AccessoriesChosen = Accessories.StereoAndLeather;
+            }
+            else if (chkStereoSystem.Checked && chkComputerNavigation.Checked && !chkLeatherInterior.Checked)
+            {
+                salesQuote.AccessoriesChosen = Accessories.StereoAndNavigation;
+            }
+            else if (chkLeatherInterior.Checked && !chkStereoSystem.Checked && chkComputerNavigation.Checked)
+            {
+                salesQuote.AccessoriesChosen = Accessories.LeatherAndNavigation;
+            }
+            else if (chkLeatherInterior.Checked && chkComputerNavigation.Checked && chkStereoSystem.Checked)
+            {
+                salesQuote.AccessoriesChosen = Accessories.All;
+            }
+            else
+            {
+                salesQuote.AccessoriesChosen = Accessories.None;
+            }
+        }
+
+        private void VehicleSummaryOutput()
+        {
+            this.lblVehicleSalePriceOutput.Text = salesQuote.VehicleSalePrice.ToString("C");
+            this.options = salesQuote.AccessoryCost + salesQuote.FinishCost;
+            lblOptionsOutput.Text = options.ToString("N");
+
+            lblSubTotalOutput.Text = salesQuote.SubTotal.ToString("C");
+            lblSalesTaxOutput.Text = salesQuote.SalesTax.ToString("N");
+            lblTotalOutput.Text = salesQuote.Total.ToString("C");
+
+            decimal tradeInAsNegative = -1m * salesQuote.TradeInAmount;
+            lblTradeInOutput.Text = tradeInAsNegative.ToString("N");
+            lblAmountDueOutput.Text = salesQuote.AmountDue.ToString("C");
+        }
+
+        private void VehicleSummaryOutputAndFinanceSummaryOutputExcludingTradeIn()
+        {
+            this.options = salesQuote.AccessoryCost + salesQuote.FinishCost;
+            lblOptionsOutput.Text = options.ToString("N");
+
+            lblSubTotalOutput.Text = salesQuote.SubTotal.ToString("C");
+            lblSalesTaxOutput.Text = salesQuote.SalesTax.ToString("N");
+            lblTotalOutput.Text = salesQuote.Total.ToString("C");
+            lblAmountDueOutput.Text = salesQuote.AmountDue.ToString("C");
+            FinancialOutput();
+        }
+
+        private void FinancialOutput()
+        {
+            this.rate = nudAnnualInterestRate.Value / 100;
+            this.paymentPeriods = (int)nudNumberOfYears.Value * 12;
+            this.presentValue = salesQuote.AmountDue;
+            this.monthlyPaymentOutput = Financial.GetPayment(rate, paymentPeriods, presentValue);
+
+            lblMonthlyPaymentFinanceOutput.Text = monthlyPaymentOutput.ToString("C");
         }
     }
 }
